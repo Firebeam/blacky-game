2d8e202083980e6d17f9d90f60845caf1c785e23
Manuel Yépez <nulike@gmail.com>
2019-04-29 13:48:31 -0600

    Initial commit

---
+++ app/src/main/java/ac/cenfotec/blackygame/GameActivity.java
@@ -0,0 +1,17 @@
+package ac.cenfotec.blackygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+    public void onCheckboxClicked(View view) {
+    }
+}
